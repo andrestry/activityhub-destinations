30d0b2e82c0e652c3ee65daf25d30a6f8ecfa952
andrestry <66209293+andrestry@users.noreply.github.com>
2026-07-06 20:31:47 +0200

    Update page.jsx

--- app/studio/[[...tool]]/page.jsx
+++ app/studio/[[...tool]]/page.jsx
@@ -6,11 +6,37 @@ import { structureTool } from 'sanity/structure'
 import { schemaTypes } from '../../../schemaTypes'
 
 const config = defineConfig({
-  projectId: 'n8tkfq9p', // <-- Paste your short Sanity Project ID here!
+  projectId: 'n8tkfq9p', // <-- Replace with your 8-character Sanity Project ID!
   dataset: 'production',
   title: 'ActivityHub Editor',
   basePath: '/studio',
-  plugins: [structureTool()],
+  plugins: [
+    structureTool({
+      structure: (S) =>
+        S.list()
+          .title('ActivityHub Content Manager')
+          .items([
+            S.listItem()
+              .title('1. Home Page')
+              .child(S.document().schemaType('homePage').documentId('homePage')),
+            S.listItem()
+              .title('2. About Us Page')
+              .child(S.document().schemaType('aboutPage').documentId('aboutPage')),
+            S.listItem()
+              .title('3. Services Page')
+              .child(S.document().schemaType('servicesPage').documentId('servicesPage')),
+            S.listItem()
+              .title('4. Destinations Page')
+              .child(S.document().schemaType('destinationsPage').documentId('destinationsPage')),
+            S.listItem()
+              .title('5. Online Platform Page')
+              .child(S.document().schemaType('platformPage').documentId('platformPage')),
+            S.listItem()
+              .title('6. Contact Us Page')
+              .child(S.document().schemaType('contactPage').documentId('contactPage')),
+          ]),
+    }),
+  ],
   schema: { types: schemaTypes },
 })
 
